370dec5a403e82f23c90a971a309b9599f99bdca
Preston Stewart Butler II <preston.butler@live.com>
2021-07-11 22:02:14 -0400

    Can now create new hero, view the hero information and navigate back and forth between the main hero list and the create form and the single hero list

--- superheroarchive/superheroes/urls.py
+++ superheroarchive/superheroes/urls.py
@@ -5,7 +5,7 @@ app_name = 'superheroes'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:superhero_id>/', views.superhero_detail, name='superhero_detail'),
-    path('new/', views.create, name='create_new_superhero')
+    path('superheroes/$', views.create, name='create_new_superhero'),
+    path('superheroes/', views.delete_hero, name='delete_hero'),
 
 ]
-
--- superheroarchive/superheroes/views.py
+++ superheroarchive/superheroes/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
+
 from .models import SuperHero
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 
 
 # Create your views here.
+
+def create(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        alter_ego = request.POST.get('alter ego')
+        primary_ability = request.POST.get('primary ability')
+        secondary_ability = request.POST.get('secondary ability')
+        catch_phrase = request.POST.get('catch phrase')
+        new_superhero = SuperHero(name=name, alter_ego=alter_ego, primary_ability=primary_ability,
+                                  secondary_ability=secondary_ability, catch_phrase=catch_phrase
+                                  )
+        new_superhero.save()
+        return HttpResponseRedirect(reverse('superheroes:index', ))
+    else:
+        return render(request, 'superheroes/create.html')
 
 
 def index(request):
@@ -16,24 +32,18 @@ def index(request):
 
 
 def superhero_detail(request, superhero_id):
-    singular_superhero = SuperHero(pk=superhero_id)
+    singular_superhero = SuperHero.objects.get(pk=superhero_id)
     context = {
         'singular_superhero': singular_superhero
     }
-    return render(request, "superheroes/detail.html", context)
+    return render(request, "superheroes/detail.html", context, )
 
 
-def create(request):
+def delete_hero(request, superhero_id):
+    singular_superhero = SuperHero.objects.get(pk=superhero_id)
     if request.method == 'POST':
-        name = request.POST.get('name')
-        alter_ego = request.POST.get('primary ability')
-        primary_power = request.POST.get('secondary ability')
-        secondary_power = request.POST.get('catch_phrase')
-        catch_phrase = request.POST.get('catch phrase')
-        new_superhero = SuperHero(name=name, alter_ego=alter_ego, primary_power=primary_power,
-                                  secondary_power=secondary_power, catch_phrase=catch_phrase
-                                  )
-        new_superhero.save()
-        return HttpResponseRedirect(reverse('superheroes:index'))
-    else:
-        return render(request, 'superheroes/create.html')
+        singular_superhero.delete()
+    context = {
+        'singular_superhero': singular_superhero
+    }
+    return render(request, "superheroes/deleteview.html", context)
